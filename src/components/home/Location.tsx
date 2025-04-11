2ff3c9d1f93221b3d74982f3b197a3c8b7eae2e7
PauloCCB <paulo.napasa3@gmail.com>
2025-04-11 01:16:42 -0500

    Se realiaza modifaciones

--- src/components/home/Location.tsx
+++ src/components/home/Location.tsx
@@ -1,17 +1,13 @@
 import Image from "next/image";
 
-import logo from "@/assets/logosaas.png";
 import SocialX from "@/assets/social-x.svg";
 import SocialInsta from "@/assets/social-insta.svg";
 import SocialLinkedIn from "@/assets/social-linkedin.svg";
-import SocialPin from "@/assets/social-pin.svg";
-import SocialYoutube from "@/assets/social-youtube.svg";
 
 const Location = () => {
   return (
     <footer className={"bg-black text-[#bcbcbc] text-sm py-10 text-center "}>
       <div className="grid grid-cols-2 container">
-
         <div className="flex flex-col text-white justify-center text-center align-middle gap-2">
           <div className="flex gap-2   justify-center pb-8 ">
             Logo
@@ -19,16 +15,14 @@ const Location = () => {
           </div>
           <p className="text-sm">gaba@gmail.com</p>
           <div className="flex justify-center gap-4 pb-4">
+            <img src="/assets/social-x.svg" alt="Twitter" width={24} height={24} />
 
-          <img src={SocialX} alt="Twitter" width={24} height={24} />
- 
-<img src={SocialInsta} alt="Twitter" width={24} height={24} />
+            <img src="/assets/social-insta.svg" alt="Twitter" width={24} height={24} />
 
-<img src={SocialLinkedIn} alt="Twitter" width={24} height={24} />
-
+            <img src="/assets/social-linkedin.svg" alt="Twitter" width={24} height={24} />
           </div>
           <div>
-            <p >Neura Sprint</p>
+            <p>Neura Sprint</p>
             <p>Calle Parque Osores 175 Dep. 504</p>
             <p>Pueblo Libre</p>
           </div>
--- src/components/layout/FooterNeura.tsx
+++ src/components/layout/FooterNeura.tsx
@@ -2,24 +2,6 @@ export const FooterNeura = () => {
   return (
       <footer className={"bg-black text-[#bcbcbc] text-sm py-10 text-center"}>
         <div className={"container"}>
-          {/* <div className={"inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:w-full before:blur before:h-full before:bg-[linear-gradient(to_right, #f87bff,#fb92cf,#ffdd9b,#c2f0b1,#2fd8fe)] before:absolute"}>
-            <Image src={logo} alt={"Logo SaaS"} height={40} className={"relative"}/>
-          </div>
-          <nav className={"flex flex-col md:flex-row md:justify-center gap-6 mt-6"}>
-            <a href={"#"}>About</a>
-            <a href={"#"}>Features</a>
-            <a href={"#"}>Customers</a>
-            <a href={"#"}>Pricing</a>
-            <a href={"#"}>Help</a>
-            <a href={"#"}>Careers</a>
-          </nav>
-          <div className={"flex justify-center gap-6 mt-6"}>
-            <SocialX />
-            <SocialInsta />
-            <SocialLinkedIn />
-            <SocialPin />
-            <SocialYoutube />
-          </div> */}
           <div className="grid grid-cols-3 space-x-2">
             <div>
               Logo
